b941f2f31be4fcb9e84c9ddc1bc37a4760c8c149
deepak  kumar <krdeep2002@gmail.com>
2026-02-27 21:01:45 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import BlogForm from "./components/BlogForm";
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/Herosection";
 import BlogById from "./pages/BlogById";
+import HeroSection from "./components/HeroSection";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <HeroSection />
+      <HeroSection/>
       <Routes>
         <Route path="/" element={<Blogs />} />
         <Route path="/blogs/:id" element={<BlogById />} />
